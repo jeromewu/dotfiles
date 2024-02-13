719833dba5d30bbaf0e3a5001d8d2ee9509806f3
Jerome Wu <jeromewus@gmail.com>
2024-02-13 05:36:58 +0000

    Enable splitjoin keymap

---
+++ .config/nvim/lua/config/splitjoin.lua
@@ -0,0 +1,5 @@
+vim.g.splitjoin_split_mapping = ''
+vim.g.splitjoin_join_mapping = ''
+
+vim.keymap.set('n', '<leader>j', ':SplitjoinJoin<cr>')
+vim.keymap.set('n', '<leader>s', ':SplitjoinSplit<cr>')
