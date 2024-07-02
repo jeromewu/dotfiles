eb45d2d14322d1dfba15b8a43a0a090ad820f2f3
jeromewu <jeromewus@gmail.com>
2024-07-02 23:29:51 +0800

    Add neo-tree

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,15 @@
 -- syntax highlight
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function () 
+    local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "lua", "norg" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
+    configs.setup({
+      ensure_installed = { "lua", "norg" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },  
+    })
+  end
 }
