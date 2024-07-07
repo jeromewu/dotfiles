2061d887369f2f7af6ea9fc8c1ad023a47dc88a9
jeromewu <jeromewus@gmail.com>
2024-07-08 00:17:35 +0800

    Add noice

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -4,14 +4,17 @@ return {
 		config = function()
 			require("gitsigns").setup()
 
-      -- display hunk in a window
+			-- display hunk in a window
 			vim.keymap.set("n", "<LEADER>gp", ":Gitsigns preview_hunk<CR>")
-      -- toggle blame
+			-- toggle blame
 			vim.keymap.set("n", "<LEADER>gb", ":Gitsigns toggle_current_line_blame<CR>")
-      -- display next hunk
+			-- display next hunk
 			vim.keymap.set("n", "<LEADER>hn", ":Gitsigns next_hunk<CR>")
-      -- display previous hunk
-			vim.keymap.set("n", "<LEADER>hp", ":Gitsigns previous_hunk<CR>")
+			-- display previous hunk
+			vim.keymap.set("n", "<LEADER>hp", ":Gitsigns prev_hunk<CR>")
 		end,
+	},
+	{
+		"sindrets/diffview.nvim",
 	},
 }
