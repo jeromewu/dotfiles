62de8531169e36656033d80daeef2990ee960c0c
jeromewu <jeromewus@gmail.com>
2024-07-07 20:54:08 +0800

    Add none-ls

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,27 +1,60 @@
 -- lsp config
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+	{
+		-- package manager for lsp, formatter and more
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
 
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({"n", "v"}, "<LEADER>ca", vim.lsp.buf.code_action, {})
-    end
-  },
+      -- display defnintion
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+      -- display code actions
+			vim.keymap.set({ "n", "v" }, "<LEADER>ca", vim.lsp.buf.code_action)
+		end,
+	},
+	{
+		-- wrap cli tools as lsp server, ex. eslint, rubocop
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.completion.spell,
+				},
+			})
+
+      -- format current buffer
+			vim.keymap.set("n", "<LEADER>gf", vim.lsp.buf.format)
+		end,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = { "stylua" },
+			})
+		end,
+	},
 }
--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,25 +1,25 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("neo-tree").setup({
-      event_handlers = {
-        {
-          -- auto close neo-tree after opening a file
-          event = "file_open_requested",
-          handler = function()
-            require("neo-tree.command").execute({ action = "close" })
-          end
-        },
-      }
-    })
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			event_handlers = {
+				{
+					-- auto close neo-tree after opening a file
+					event = "file_open_requested",
+					handler = function()
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+		})
 
-    -- toggle neo-tree
-    vim.keymap.set("n", "<LEADER>nt", ":Neotree toggle=true<CR>")
-  end
+		-- toggle neo-tree
+		vim.keymap.set("n", "<LEADER>nt", ":Neotree toggle=true<CR>")
+	end,
 }
