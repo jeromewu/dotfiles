b5ecbee240df1c0aee8318ba1b8cd4295359c823
Jerome Wu <jeromewus@gmail.com>
2024-07-25 07:12:40 +0000

    feat: add harpoon

---
+++ .config/nvim/lua/plugins/file-management.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("neo-tree").setup({
+        event_handlers = {
+          {
+            -- auto close neo-tree after opening a file
+            event = "file_open_requested",
+            handler = function()
+              require("neo-tree.command").execute({ action = "close" })
+            end,
+          },
+        },
+      })
+
+      vim.keymap.set("n", "<LEADER>nt", ":Neotree toggle=true<CR>", {
+        silent = true,
+        desc = "Toggle Neotree",
+      })
+    end,
+  },
+  {
+    "ThePrimeagen/harpoon",
+    config = function()
+      vim.keymap.set("n", "<LEADER>ha", ':lua require("harpoon.mark").add_file()<CR>', {
+        silent = true,
+        desc = "Add file",
+      })
+      vim.keymap.set("n", "<LEADER>hm", ':lua require("harpoon.ui").toggle_quick_menu()<CR>', {
+        silent = true,
+        desc = "Display added files",
+      })
+    end,
+  },
+}
