970bd87e0ecd8fe87be2832a0530412a19ac3480
Jerome Wu <jeromewus@gmail.com>
2024-07-25 08:29:22 +0000

    feat: disable scope in ibl

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -22,7 +22,11 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     config = function()
-      require("ibl").setup()
+      require("ibl").setup({
+        scope = {
+          enabled = false
+        }
+      })
     end,
   },
 }
