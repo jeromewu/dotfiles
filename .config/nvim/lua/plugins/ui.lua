c28116dad4ef6151dca7108fe3b4e242a61285d0
Jerome Wu <jeromewus@gmail.com>
2024-07-25 07:28:12 +0000

    feat: add backline

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,21 +1,28 @@
 return {
-	-- color theme
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				integrations = {
-					neotree = true,
-					noice = true,
-					diffview = true,
-					mason = true,
-					notify = true,
-				},
-			})
+  -- color theme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        integrations = {
+          neotree = true,
+          noice = true,
+          diffview = true,
+          mason = true,
+          notify = true,
+        },
+      })
 
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = function()
+      require("ibl").setup()
+    end,
+  },
 }
