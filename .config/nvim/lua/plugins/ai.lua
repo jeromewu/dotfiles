58f3fb4d6b0bf18cfb37239d521f501bb7bd8084
jeromewu <jeromewus@gmail.com>
2024-08-03 22:23:44 +0800

    feat: add gp.nvim with gemini config

---
+++ .config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "robitx/gp.nvim",
+    config = function()
+      local conf = {
+        providers = {
+          googleai = {
+            disable = false,
+            endpoint =
+            "https://generativelanguage.googleapis.com/v1beta/models/{{model}}:streamGenerateContent?key={{secret}}",
+            secret = os.getenv("GOOGLEAI_API_KEY"),
+          },
+        },
+      }
+      require("gp").setup(conf)
+    end,
+  },
+}
