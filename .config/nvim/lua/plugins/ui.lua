dfa87f615354a7c22a14abeb498c9514bb59ef1a
jeromewu <jeromewus@gmail.com>
2024-08-17 15:19:04 +0800

    feat: add alpha.nvim

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -33,4 +33,13 @@ return {
       })
     end,
   },
+  {
+    "goolord/alpha-nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local startify = require("alpha.themes.startify")
+      startify.file_icons.provider = "devicons"
+      require("alpha").setup(startify.config)
+    end,
+  },
 }
