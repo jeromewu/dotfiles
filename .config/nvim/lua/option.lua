97fb807d7fdc769a133fdbb72382508105cd98ce
Jerome Wu <jeromewus@gmail.com>
2024-08-17 21:32:30 +0800

    feat: enhance tab experience.

--- .config/nvim/lua/option.lua
+++ .config/nvim/lua/option.lua
@@ -6,3 +6,6 @@ vim.opt.shiftwidth = 2
 -- use spaces instead of tabs
 vim.opt.expandtab = true
 vim.opt.termguicolors = true
+-- better tab experience
+vim.opt.wildmode = "longest,list,full"
+vim.opt.wildmenu = true
