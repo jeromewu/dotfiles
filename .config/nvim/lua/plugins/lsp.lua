52955d024613e57a404a3c471bfcee06dd12f456
Jerome Wu <jeromewus@gmail.com>
2024-08-17 22:05:28 +0800

    feat: merge lua files to simply workflow

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,3 @@
--- lsp config
 return {
 	{
 		-- package manager for lsp, formatter and more
@@ -66,6 +65,50 @@ return {
 		config = function()
 			require("mason-null-ls").setup({
 				ensure_installed = { "stylua", "typos-lsp" },
+			})
+		end,
+	},
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		-- snippet engine written in lua
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			-- a collection of snippets
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+				}),
 			})
 		end,
 	},
