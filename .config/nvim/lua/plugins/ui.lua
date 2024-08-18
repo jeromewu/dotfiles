85a2a4a581e9fdc876dd4f7ed6ef6a26bb7a0c9b
Jerome Wu <jeromewus@gmail.com>
2024-08-18 15:25:09 +0800

    feat: fix indent

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,82 +1,82 @@
 return {
-	-- color theme
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				integrations = {
-					neotree = true,
-					noice = true,
-					diffview = true,
-					mason = true,
-					notify = true,
-					harpoon = true,
-					nvim_surround = true,
-					which_key = true,
-				},
-			})
+  -- color theme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        integrations = {
+          neotree = true,
+          noice = true,
+          diffview = true,
+          mason = true,
+          notify = true,
+          harpoon = true,
+          nvim_surround = true,
+          which_key = true,
+        },
+      })
 
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		config = function()
-			require("ibl").setup({
-				scope = {
-					show_start = false,
-					show_end = false,
-				},
-			})
-		end,
-	},
-	{
-		"goolord/alpha-nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			local startify = require("alpha.themes.startify")
-			startify.file_icons.provider = "devicons"
-			require("alpha").setup(startify.config)
-		end,
-	},
-	-- better cmdline, notify and more
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
-		config = function()
-			require("noice").setup({
-				lsp = {
-					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-					override = {
-						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-						["vim.lsp.util.stylize_markdown"] = true,
-						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-					},
-				},
-				presets = {
-					bottom_search = true, -- use a classic bottom cmdline for search
-					command_palette = true, -- position the cmdline and popupmenu together
-					long_message_to_split = true, -- long messages will be sent to a split
-					inc_rename = false, -- enables an input dialog for inc-rename.nvim
-					lsp_doc_border = false, -- add a border to hover docs and signature help
-				},
-			})
-		end,
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = {
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = function()
+      require("ibl").setup({
+        scope = {
+          show_start = false,
+          show_end = false,
+        },
+      })
+    end,
+  },
+  {
+    "goolord/alpha-nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local startify = require("alpha.themes.startify")
+      startify.file_icons.provider = "devicons"
+      require("alpha").setup(startify.config)
+    end,
+  },
+  -- better cmdline, notify and more
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        presets = {
+          bottom_search = true,    -- use a classic bottom cmdline for search
+          command_palette = true,  -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false,      -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false,  -- add a border to hover docs and signature help
+        },
+      })
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
-		config = function()
-			require("lualine").setup()
-		end,
-	},
+    config = function()
+      require("lualine").setup()
+    end,
+  },
 }
--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -1,76 +1,76 @@
 return {
-	{
-		"alexghergh/nvim-tmux-navigation",
-		config = function()
-			local nvim_tmux_nav = require("nvim-tmux-navigation")
+  {
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      local nvim_tmux_nav = require("nvim-tmux-navigation")
 
-			nvim_tmux_nav.setup({
-				disable_when_zoomed = true, -- defaults to false
-			})
+      nvim_tmux_nav.setup({
+        disable_when_zoomed = true, -- defaults to false
+      })
 
-			vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-			vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-			vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-			vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
-			vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
-			vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
-		end,
-	},
+      vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+      vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+      vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+      vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+      vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+      vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+    end,
+  },
   -- keymap documentation
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		config = function()
-			local wk = require("which-key")
-			wk.add({
-				{
-					"<LEADER>?",
-					function()
-						require("which-key").show({ global = false })
-					end,
-					desc = "Buffer Local Keymaps (which-key)",
-				},
-				{ "<LEADER>c", group = "Code" },
-				{ "<LEADER>d", group = "Diffview" },
-				{ "<LEADER>f", group = "Find" },
-				{ "<LEADER>g", group = "Git" },
-				{ "<LEADER>h", group = "Hunk/Harpoon" },
-				{ "<LEADER>n", group = "NeoTree/Neorg" },
-				{ "<LEADER>t", group = "Tab" },
-				{ "<LEADER>x", group = "Diagnostics" },
-			})
-		end,
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope-ui-select.nvim",
-		},
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+      local wk = require("which-key")
+      wk.add({
+        {
+          "<LEADER>?",
+          function()
+            require("which-key").show({ global = false })
+          end,
+          desc = "Buffer Local Keymaps (which-key)",
+        },
+        { "<LEADER>c", group = "Code" },
+        { "<LEADER>d", group = "Diffview" },
+        { "<LEADER>f", group = "Find" },
+        { "<LEADER>g", group = "Git" },
+        { "<LEADER>h", group = "Hunk/Harpoon" },
+        { "<LEADER>n", group = "NeoTree/Neorg" },
+        { "<LEADER>t", group = "Tab" },
+        { "<LEADER>x", group = "Diagnostics" },
+      })
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
 
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<LEADER>ff", builtin.find_files, {
-				desc = "Find by file names",
-			})
-			vim.keymap.set("n", "<LEADER>fg", builtin.live_grep, {
-				desc = "Find in files",
-			})
-			vim.keymap.set("n", "<LEADER>fb", builtin.buffers, {
-				desc = "Find in buffers",
-			})
-			vim.keymap.set("n", "<LEADER>fh", builtin.help_tags, {
-				desc = "Find in help docs",
-			})
-		end,
-	},
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<LEADER>ff", builtin.find_files, {
+        desc = "Find by file names",
+      })
+      vim.keymap.set("n", "<LEADER>fg", builtin.live_grep, {
+        desc = "Find in files",
+      })
+      vim.keymap.set("n", "<LEADER>fb", builtin.buffers, {
+        desc = "Find in buffers",
+      })
+      vim.keymap.set("n", "<LEADER>fh", builtin.help_tags, {
+        desc = "Find in help docs",
+      })
+    end,
+  },
 }
