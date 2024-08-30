c18450855fc01ac5a45deea9391ccd7a0dfca2da
Jerome Wu <jeromewus@gmail.com>
2024-08-30 09:54:02 +0800

    feat: update nvim-cmp keybindings and update journal config

--- .config/nvim/lua/plugins/code-ts-lsp.lua
+++ .config/nvim/lua/plugins/code-ts-lsp.lua
@@ -102,6 +102,7 @@ return {
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load()
 
+      local luasnip = require("luasnip")
       local cmp = require("cmp")
       cmp.setup({
         snippet = {
@@ -119,6 +120,24 @@ return {
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<C-n>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<C-p>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
--- .config/nvim/lua/plugins/note.lua
+++ .config/nvim/lua/plugins/note.lua
@@ -15,6 +15,11 @@ return {
         load = {
           ["core.defaults"] = {},
           ["core.concealer"] = {},
+          ["core.journal"] = {
+            config = {
+              workspace = "notes",
+            }
+          },
           ["core.completion"] = {
             config = {
               engine = "nvim-cmp",
@@ -35,15 +40,15 @@ return {
         silent = true,
         desc = "Open note",
       })
-      vim.keymap.set("n", "<LEADER>jj", ":Neorg workspace notes<CR> <BAR> :Neorg journal today<CR>", {
+      vim.keymap.set("n", "<LEADER>jj", ":Neorg journal today<CR>", {
         silent = true,
         desc = "Open journal (today)",
       })
-      vim.keymap.set("n", "<LEADER>jy", ":Neorg workspace notes<CR> <BAR> :Neorg journal yesterday<CR>", {
+      vim.keymap.set("n", "<LEADER>jy", ":Neorg journal yesterday<CR>", {
         silent = true,
         desc = "Open journal (yesterday)",
       })
-      vim.keymap.set("n", "<LEADER>jt", ":Neorg workspace notes<CR> <BAR> :Neorg journal tomorrow<CR>", {
+      vim.keymap.set("n", "<LEADER>jt", ":Neorg journal tomorrow<CR>", {
         silent = true,
         desc = "Open journal (tomorrow)",
       })
